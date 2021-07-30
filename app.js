a59ebd4753eceee175dea32ee7719cb130f66907
isa <sumerisa5@gmail.com>
2021-07-30 14:29:56 +0300

    html, css and js filed created. to do list web application designed

---
+++ app.js
@@ -0,0 +1,20 @@
+let date = document.querySelector("#date");
+date.innerHTML = new Date().getDate()+'-'+(new Date().getMonth()+1)+'-'+new Date().getFullYear();
+
+let liste = document.querySelector(".liste")
+var urun = document.querySelector("#veri"); 
+var ekle = document.querySelector("#ekle")
+
+ekle.addEventListener("click",function(){
+    // createElement ile yeni bir listItem (li) oluşturuyoruz
+    var li=document.createElement("li"); 
+    
+    // Input ile gelen veriyi "li" içerisine metin olarak giriyoruz.
+    li.textContent=veri.value; 
+    
+    // appendChild ile oluşturduğumuz elementi "liste" isimli değişkenimizin içerisine ekliyoruz.
+    liste.appendChild(li);
+                
+    // Veri inputu içerisindeki metni siliyoruz.
+    veri.value = "";
+  });
